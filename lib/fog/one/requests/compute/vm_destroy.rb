870dfdbab6d7dd5955897f7ae01f4ebe51bde7a0
Achim Ledermüller <achim.ledermueller@netways.de>
2017-04-12 19:45:25 +0200

    [vm_destroy] only destroy server if opennebula returns exactly 1 vm

--- lib/fog/one/requests/compute/vm_destroy.rb
+++ lib/fog/one/requests/compute/vm_destroy.rb
@@ -4,12 +4,12 @@ module Fog
       class Real
         def vm_destroy(id)
           vmpool = ::OpenNebula::VirtualMachinePool.new(client)
-	  vmpool.info!(-2,id,id,-1)
-          
-	  vmpool.each do |vm|
-            # true => delete and recreate vm
-	    vm.delete(false)
-          end
+          vmpool.info!(-2,id,id,-1)
+
+          raise ArgumentError, "Could not delete server with #{id}. Found #{vmpool.count} server with id: #{id}" if vmpool.count != 1
+
+          # true => delete and recreate vm
+          vmpool.first.delete(false)
         end
       end
       class Mock
