8805a97a39db40da1bc1b76f0fbf9a201ad374d3
Achim Ledermüller <achim.ledermueller@netways.de>
2017-04-12 19:46:47 +0200

    [servers] raise error in get if opennebula returns more than 1 vm

--- lib/fog/one/models/compute/servers.rb
+++ lib/fog/one/models/compute/servers.rb
@@ -15,8 +15,18 @@ module Fog
         end
 
         def get(id)
+          raise ArgumentError, 'Fog:Compute:One:Servers.get: Argument nil is not allowed' if id.nil?
           data = service.list_vms({:id => id})
-          new data.first unless data.empty?
+
+          # raise exception if we found more than one vm
+          raise ArgumentError, "OpenNebula returned #{data.length} elements. Should be 0 or 1" if data.length > 1
+
+          # return the server object if we found one vm
+          new data.first data.length == 1
+
+          # return data (empty array) if we found no vm
+          data
+
         end
 
         def shutdown(id)
